530163ffb652d86b04fe187b5184192a9af2650b
Kamal Nayan <mailtokamalnayan@gmail.com>
2021-05-10 07:49:53 +0200

    Add first version of a basic home page (#1)

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,31 +1,25 @@
 import React from "react";
+import "../Home.css";
 
 const Home = () => {
   return (
-    <div className="container mx-auto">
-      <h1 className="text-5xl text-gray-400">
-        {"Welcome to Priyanshu 's boilerplate"}
-      </h1>
-      <ul className="mt-8 list-disc ml-8">
-        <li>
-          React Router - Click on <span className="font-bold">New Page</span> to
-          go to new Page
-        </li>
-        <li>
-          {" "}
-          <span className="text-purple-700 text-opacity-75">Tailwind</span> CSS
-        </li>
-        <li>Live reloading</li>
-        <li>
-          {" "}
-          <span className="text-red-700">Eslint </span>
-        </li>
-        <li>Prettier</li>
-        <li>
-          No config Bundler -{" "}
-          <span className="text-yellow-700 text-opacity-75">Parcel</span>
-        </li>
-      </ul>
+    <div className="pageBackground">
+      <main className="container px-4 lg:px-8 mx-auto min-h-screen flex flex-wrap content-center max-w-5xl">
+        <h1 className="text-3xl lg:text-5xl mb-4 lg:mb-6 z-10 text-white">
+          <span className="font-bold">Find movies. </span> <br></br>
+          <span className="font-light">Together.</span>
+        </h1>
+        <h2 className="text-lg lg:text-2xl mb-6 lg:mb-8 font-light max-w-5xl z-10 text-white">
+          Decide what to watch by starting a movie finding session. Set your
+          preferences and find movies your both like, together.
+        </h2>
+        <a
+          href=""
+          className="bg-gray-800 p-3 text-white rounded z-10 font-semibold"
+        >
+          Start now →
+        </a>
+      </main>
     </div>
   );
 };
