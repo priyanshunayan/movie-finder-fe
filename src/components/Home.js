23f4dc08dbd4519f4254943ef5287f38f6eea46c
Kamal Nayan <mailtokamalnayan@gmail.com>
2021-05-26 18:29:25 +0200

    Minor UI enhancements and tailwind config

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,24 +1,31 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import "../Home.css";
+import Illustration from "../assets/images/illustration.svg";
+import "../assets/css/Home.css";
 
 const Home = () => {
   return (
     <div className="pageBackground">
       <main className="container px-4 lg:px-8 mx-auto min-h-screen flex flex-wrap content-center max-w-5xl">
-        <h1 className="text-3xl lg:text-5xl mb-4 lg:mb-6 z-10 text-white">
-          <span className="font-bold">Find movies. </span> <br></br>
-          <span className="font-light">Together.</span>
-        </h1>
-        <h2 className="text-lg lg:text-2xl mb-6 lg:mb-8 font-light max-w-5xl z-10 text-white">
-          Decide what to watch by starting a movie finding session. Set your
-          preferences and find movies your both like, together.
-        </h2>
+        <img src={Illustration} className="mb-8 w-48 block" />
+        <div>
+          <h1 className="text-3xl lg:text-5xl mb-4 lg:mb-6 z-10 tracking-tight">
+            <span className="font-bold block lg:mb-2">
+              Find movies to watch.
+            </span>
+            <span>Together.</span>
+          </h1>
+          <h2 className="text-lg lg:text-2xl mb-6 lg:mb-8 max-w-5xl z-10 tracking-tight lg:leading-relaxed">
+            Decide what to watch by starting a movie finding session. Set your
+            preferences and find movies your both like, together.
+          </h2>
+        </div>
+
         <Link
           to="/filters"
-          className="bg-gray-800 p-3 text-white rounded z-10 font-semibold"
+          className="bg-gray-800 px-4 py-2 text-white rounded z-10 font-semibold lg:text-lg"
         >
-          Start now →
+          Start a session
         </Link>
       </main>
     </div>
